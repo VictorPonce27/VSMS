3de2160986ad88f289db2f32a7656dd83a291156
Victor Ponce <A008273@itesm.mx>
2020-08-30 06:04:22 -0500

    Mongo file testing

--- mongo.py
+++ mongo.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient, errors
 import pymongo
 from pymongo import MongoClient
 
@@ -8,36 +9,51 @@ import os
 
 cluster = pymongo.MongoClient("mongodb+srv://victor:8246@cluster1.i5pf9.mongodb.net/Discrod?retryWrites=true & w=majority")
 
-post = {"_id":0, "name": "tim","score":5, "Major":0}
-post1 = {"_id":5, "name": "joe"}
-post2 = {"_id":6, "name": "bill"}
 
-#local_dir = os.getcwd()
+db1 = cluster['MajorsDatabase']
 
-#Para torvic
-#numa torvic
+dbu = cluster['UserData']
+collectionu = dbu['data']
 
-#table = pd.read_csv(f'{local_dir}/ClasesFinalFINAL.csv')
-# print(table)
+df = ["Ambiente Construido", "Ciencias Sociales",
+      "Estudios Creativos", "Negocios", "Salud", "Innovacion y transformacion", 
+      "Computacion y Tecnologias de Informacion", "Bioingenieria y Procesos Quimicos",
+      "Ciencias aplicadas"]
 
-db = cluster['MajorsDatabase']
-collection = db["Ambiente Construido"]
+"""
+database_names() AND collection_names ARE DEPRECATED
+"""
+# collection = db["Ambiente Construido"]
+database_names = cluster.list_database_names()
 
-myquery = {"CarreraID": 0 }
-mydoc = collection.find(myquery)
+# iterate over the list of database names
+for db_num, db in enumerate(database_names):
+    # use the list_collection_names() method to return collection names
+    collection_names = cluster[db].list_collection_names()
+    if(db == "MajorsDatabase"):
+        # iterate over the list of collection names
+        for col in df:
+            print(col, "--")
+            collection = db1[col]
+            myquery = {"major":col}
+            mydoc = collection.find(myquery)
+            aux= []
+            count = 0
+            for i in mydoc: 
+                myquery2 = {"major":i["major"]}
+                mydoc2 = collectionu.find(myquery2)
+                for j in mydoc2:
+                    print({'classID': i['classID']})
+                    if(i["classID"] == j["class"]):
+                        print("inside")
+                        print({'class':i['class']})
+                        print(" ")
+                        print({'class':j['class']})
+                        aux.append(j['username'])
+                        count = count + 1 
+                print(len(aux))
+                if(len(aux) == 5):
+                    print(aux)
 
-for i in mydoc:
-    print(i['class'])
-
-# for i in range(0,9,1):
-#     print(" ")
-#     print(table.columns[i])
-#     collection = db[f'{table.columns[i]}']
-#     num = 0
-#     for j in range(0, table.count(axis='columns')[i], 1):
-#         print(table.iloc[j][i])
-#         post = {"CarreraID":i,"ClassID":num,"Mayor":table.columns[i],"class":table.iloc[j][i],}
-#         collection.insert_one(post)
-#         num = num+1
 
 
